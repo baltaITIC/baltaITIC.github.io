b64938806ca6794f6dea2181c49d77914e312088
Baltazar Muñoz Ledo Hurtado <bmledo@scio.local>
2018-08-15 17:47:19 -0500

    Second commit

--- getUsers.js
+++ getUsers.js
@@ -7,7 +7,7 @@ $.getJSON( "https://jsonplaceholder.typicode.com/users", function( data ) {
 
 function addCard(val) {
 
-  return result ="<div class='card' style='width: 20%; display: inline-block; border: 3px dashed #8c8b8b; margin:10px 10px 10px 10px; padding:10px;'>\
+  return result ="<div class='col col-md-3 col-lg-3 col-sm-6  border border-primary' style='display: inline-block;'>\
   <div class='card-body border border-info rounded'>\
     <h3 class='card-title'>"+val.name+"</h3>\
     <h6 class='card-subtitle mb-2 text-muted'>"+val.username+"</h6>\
@@ -15,8 +15,15 @@ function addCard(val) {
       Street: "+val.address.street+"<br/>\
       Suite: "+val.address.suite+"<br/>\
       City: "+val.address.city+" </p>\
-    <a href='#' class='btn btn-primary'>Interesting</a>\
+    <a href='#' class='btn btn-primary'>Interesting</a> <hr>\
   </div>\
 </div>";
 
 }
+  $('#space').height($('header').height()+20);
+
+  if ('serviceWorker' in navigator) {
+     navigator.serviceWorker
+              .register('./service-worker.js')
+              .then(function() { console.log('Service Worker Registered'); });
+   }
