4233d8e18d8d0c6962714106b09bfe13961c22d6
Baltazar Muñoz Ledo Hurtado <bmledo@scio.local>
2018-08-16 09:01:45 -0500

    PWA builder try

--- getUsers.js
+++ getUsers.js
@@ -22,8 +22,14 @@ function addCard(val) {
 }
   $('#space').height($('header').height()+20);
 
-  if ('serviceWorker' in navigator) {
-     navigator.serviceWorker
-              .register('./service-worker.js')
-              .then(function() { console.log('Service Worker Registered'); });
-   }
+  //Register service worker
+
+  if (navigator.serviceWorker.controller) {
+    console.log('[PWA Builder] active service worker found, no need to register')
+  } else {
+    navigator.serviceWorker.register('pwabuilder-sw.js', {
+      scope: './'
+    }).then(function(reg) {
+      console.log('Service worker has been registered for scope:'+ reg.scope);
+    });
+  }
