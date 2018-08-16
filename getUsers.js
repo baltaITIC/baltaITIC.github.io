7de093218638376ef550de3e19733a988d7b761b
Baltazar Muñoz Ledo Hurtado <bmledo@scio.local>
2018-08-16 12:43:38 -0500

    Second commit

--- getUsers.js
+++ getUsers.js
@@ -38,7 +38,7 @@ function addCard(val) {
 
   window.addEventListener('beforeinstallprompt', function(e) {
   e.userChoice.then(function(choiceResult) {
-
+    alert("Funciona");
     console.log(choiceResult.outcome);
 
     if(choiceResult.outcome == 'dismissed') {
